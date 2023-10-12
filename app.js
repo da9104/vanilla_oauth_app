20053c455e2e30c86fefc886ea8a796fe10450d3
DAMI <damikang@DAMIs-MacBook-Pro.local>
2023-10-12 18:50:43 +0100

    app.js google callbackURL adjusted.

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:5005/auth/google/secret",
+    callbackURL: "https://auth-od9d.onrender.com/auth/google/secret",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function(accessToken, refeshToken, profile, cb) {
